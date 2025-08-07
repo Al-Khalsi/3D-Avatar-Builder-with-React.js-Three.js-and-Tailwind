4ddcb76bae417aca387ffe36360bfb62cdb7a996
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2025-08-07 14:35:46 +0330

    refactor(App): Improved page code and imported main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { MainLayout } from "./layout/MainLayout";
+import Avatar from "./components/Avatar";
 
 function App() {
   return (
-    <>
-      <Canvas camera={{ position: [3, 3, 3] }}>
+    <MainLayout>
+      <Canvas camera={{ position: [3, 3, 3], fov: 50 }} className="z-0">
         <color attach="background" args={["#333333"]} />
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
         <OrbitControls />
-        <mesh>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshNormalMaterial />
-        </mesh>
+        <Avatar />
       </Canvas>
-    </>
-  )
+    </MainLayout>
+  );
 }
 
-export default App
+export default App;
